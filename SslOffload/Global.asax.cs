c27747b6da069840140a92724215de7ceb89f30b
Shawn Neal <sneal@sneal.net>
2017-01-25 08:22:34 -0800

    Only force SSL if SSL offload

--- SslOffload/Global.asax.cs
+++ SslOffload/Global.asax.cs
@@ -11,9 +11,14 @@ namespace SslOffload
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            // This forces the IIS7WorkerRequest to generate a https URL
-            // https://github.com/Microsoft/referencesource/blob/4fe4349175f4c5091d972a7e56ea12012f1e7170/System.Web/Hosting/IIS7WorkerRequest.cs#L402
-            HttpContext.Current.Request.ServerVariables["HTTPS"] = "on";
+            // SSL offload?
+            if ("https".Equals(HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_PROTO"],
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                // This forces the IIS7WorkerRequest to generate a https URL
+                // https://github.com/Microsoft/referencesource/blob/4fe4349175f4c5091d972a7e56ea12012f1e7170/System.Web/Hosting/IIS7WorkerRequest.cs#L402
+                HttpContext.Current.Request.ServerVariables["HTTPS"] = "on";
+            }
         }
     }
 }
